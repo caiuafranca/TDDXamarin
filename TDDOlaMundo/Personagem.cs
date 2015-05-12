98a6d13bb3a7abeb0d05900350847812d0d4ec2c
Caiua Franca <caiuagomes@gmail.com>
2015-05-12 16:00:27 -0300

    Refatorando o Codigo

--- TDDOlaMundo/Personagem.cs
+++ TDDOlaMundo/Personagem.cs
@@ -8,6 +8,7 @@ namespace TDDOlaMundo
 	public class Personagem
 	{
 		public int Life;
+		public int atack;
 		public int vidas;
 		public List<Itens> mochila = new List<Itens>();
 
@@ -15,13 +16,16 @@ namespace TDDOlaMundo
 		public Personagem(){
 			this.Life = 80;
 			this.vidas = 5;
+			this.atack = 10;
 			this.mochila.Clear();
 		}
 
-		public void comer(Itens item){
-			
+		public void aumentarLife(Itens item){			
 			this.Life += item.life;
+		}
 
+		public void adicionarItensMochila(Itens item){
+			this.mochila.Add (item);
 		}
 
 	}
--- TDDOlaMundo/Test.cs
+++ TDDOlaMundo/Test.cs
@@ -6,40 +6,51 @@ namespace TDDOlaMundo
 	[TestFixture ()]
 	public class Test
 	{
+		Personagem personagem;
+
+		[SetUp()]
+		public void CriarPersonagem(){
+			personagem = new Personagem ();
+		}
+
 		[Test ()]
 		public void CriarPersonagemComLife80()
 		{
-			Personagem personagem = new Personagem ();
+			//Personagem personagem = new Personagem ();
 
 			Assert.AreEqual (80, personagem.Life);
 		}
 
 		[Test ()]
 		public void CriarPersonagemcom5vidas(){
-		
-			Personagem personagem = new Personagem ();
 			Assert.AreEqual (5, personagem.vidas);
 		}
 		[Test ()]
 		public void MochilaVazia(){
-			Personagem personagem = new Personagem ();
 			Assert.IsEmpty(personagem.mochila);
 		}
 
 		[Test ()]
 		public void AdicionarItensAMochila(){
-			Personagem personagem = new Personagem ();
-			personagem.mochila.Add (new Itens ("faca", "Arma", 10));
+
+			personagem.adicionarItensMochila (new Itens ("faca", "Arma", 10));
+			personagem.adicionarItensMochila (new Itens ("Espada Vorpal", "Arma", 100));
+			personagem.adicionarItensMochila (new Itens ("Maçã", "Alimento", 10));
+			personagem.adicionarItensMochila (new Itens ("Fruta Sagrada", "Alimento", 100));
+
 			Assert.IsNotEmpty (personagem.mochila);
 		}
 
 		[Test ()]
 		public void AoComerAlimentoAumentaoLife(){
-			Personagem personagem = new Personagem ();
+			
 			personagem.mochila.Add(new Itens("pera","alimento", 5));
 			personagem.mochila.Add(new Itens("Fruta Sagrada","alimento", 30));
+
 			var lifeAtual = personagem.Life;
-			personagem.comer (new Itens("pera","alimento",5));
+
+			personagem.aumentarLife (new Itens("pera","alimento",5));
+
 			Assert.IsTrue (lifeAtual < personagem.Life);
 		}
 
